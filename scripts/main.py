79a38af6f8407422d30173931d49400cda427d39
Farouk <farouk.mokhtar@gmail.com>
2021-02-19 13:55:26 +0200

    typo

--- scripts/main.py
+++ scripts/main.py
@@ -283,7 +283,7 @@ if __name__ == "__main__":
 
         # test the model
         if args.test:
-            for epoch in args.num_epoch:
+            for epoch in range(args.num_epoch):
                 # load the model per epoch to be tested
                 outpath = args.outpath + args.load_model
                 PATH = outpath + '/epoch_' + str(epoch)+1 + '_weights.pth'
