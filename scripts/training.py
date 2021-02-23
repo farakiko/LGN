d7e52ca2006d323e8dc57fc804193c85a3cabab5
Farouk <farouk.mokhtar@gmail.com>
2021-02-23 09:57:05 +0200

    gpu check

--- scripts/training.py
+++ scripts/training.py
@@ -48,7 +48,7 @@ def train(args, model, loader, optimizer, lr, epoch, device, outpath):
         t0 = time.time()
 
         # for better reading of the code
-        X = batch
+        X = batch.to(device)
         Y = batch['is_signal'].to(device)
 
         # forwardprop
