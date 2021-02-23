97c2ab760dd192b4601bf0e99967f99911b75108
Farouk <farouk.mokhtar@gmail.com>
2021-02-23 11:16:28 +0200

    fixed issue

--- scripts/args.py
+++ scripts/args.py
@@ -25,8 +25,7 @@ def setup_argparse():
     parser.add_argument("--test", action=BoolArg, default=True, help="Test the model.. make roc curves & confusion matrix")
     parser.add_argument("--load-model", type=str, default='LGNTopTag_model#four_epochs_batch32_with_acc', help="Path to the model to be loaded")
     parser.add_argument("--load-epoch", type=int, default=0, help="Specefication of which epoch to load. Set to 0 to test over all the epochs the model has trained on.")
-    parser.add_argument('--batch-size-test', '-bs', type=int, default=8, metavar='N',
-                        help='Batch size for test_loader.')
+    parser.add_argument('--batch-size-test', type=int, default=8, metavar='N', help='Batch size for test_loader.')
 
     # Optimizer options
     parser.add_argument('--num-epoch', type=int, default=1, metavar='N',
